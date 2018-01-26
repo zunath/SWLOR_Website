8ce5bddc7bee81498b44f3dbc739441775474b24
zunath <coolty3001@yahoo.com>
2018-01-26 21:14:30 +0800

    Stand up intro, tweak menu bar.

--- SWLOR.Web/ClientApp/components/Header.tsx
+++ SWLOR.Web/ClientApp/components/Header.tsx
@@ -2,7 +2,7 @@
 import { Link } from 'react-router-dom';
 import * as dotnetify from 'dotnetify';
 import Logout from './Logout';
-import * as Logo from '../images/compressed/swollogo2.png';
+import * as Logo from '../images/swollogo2.png';
 
 export default class Header extends React.Component<any, any> {
     vm: any;
@@ -34,7 +34,7 @@ export default class Header extends React.Component<any, any> {
             if (self.state.Role === 1 || self.state.Role === 2) {
                 return <li className="nav-item">
                     <Link className="nav-link" to="/Admin">
-                        <i className="fa fa-cogs fa-lg" /> Admin
+                        <i className="fa fa-cogs " /> Admin
                     </Link>
                 </li>;
             }
@@ -56,65 +56,79 @@ export default class Header extends React.Component<any, any> {
 
                     <div className="navbar">
                         <ul className="navbar-nav">
+
                             <li className="nav-item">
-                                <Link className="nav-link" to="/story">
-                                    <i className="fa fa-info-circle fa-lg" /> Story
+                                <Link className="nav-link" to="/home">
+                                    <i className="fa fa-home " /> Home
                                 </Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/classes">
-                                    <i className="fa fa-info-circle fa-lg" /> Classes
+                            <li className="nav-item dropdown">
+                                <Link id="loreDropdown" className="nav-link dropdown-toggle" to="#" data-toggle="dropdown" role="button">
+                                    <i className="fa fa-space-shuttle " /> Lore
                                 </Link>
+                                <div className="dropdown-menu">
+                                    <Link className="dropdown-item" to="/story">
+                                        <i className="fa fa-book " /> Story
+                                    </Link>
+                                    <Link className="dropdown-item" to="/races">
+                                        <i className="fa fa-male " /> Races
+                                    </Link>
+                                </div>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/races">
-                                    <i className="fa fa-info-circle fa-lg" /> Races
+                            <li className="nav-item dropdown">
+                                <Link id="gameplayDropdown" className="nav-link dropdown-toggle" to="#" data-toggle="dropdown" role="button">
+                                    <i className="fa fa-gamepad " /> Gameplay
                                 </Link>
+                                <div className="dropdown-menu">
+                                    <Link className="dropdown-item" to="/classes">
+                                        <i className="fa fa-shield " /> Classes
+                                    </Link>
+                                    <Link className="dropdown-item" to="/skills">
+                                        <i className="fa fa-bolt " /> Skills
+                                    </Link>
+                                </div>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/crafting">
-                                    <i className="fa fa-info-circle fa-lg" /> Skills
+                            <li className="nav-item dropdown">
+                                <Link id="serverInfoDropdown" className="nav-link dropdown-toggle" to="#" data-toggle="dropdown" role="button">
+                                    <i className="fa fa-info " /> Server Info
                                 </Link>
+                                <div className="dropdown-menu">
+                                    <Link className="dropdown-item" to="/downloads">
+                                        <i className="fa fa-download " /> Downloads
+                                    </Link>
+                                    <Link className="dropdown-item" to="/connection-info">
+                                        <i className="fa fa-plug " /> Connection Info
+                                    </Link>
+                                </div>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/crafting">
-                                    <i className="fa fa-info-circle fa-lg" /> Crafting
+                            <li className="nav-item dropdown">
+                                <Link id="communityDropdown" className="nav-link dropdown-toggle" to="#" data-toggle="dropdown" role="button">
+                                    <i className="fa fa-users " /> Community
                                 </Link>
+                                <div className="dropdown-menu">
+                                    <Link className="dropdown-item" to="http://forums.starwarsnwn.com/">
+                                        <i className="fa fa-list " /> Forums
+                                    </Link>
+                                    <Link className="dropdown-item" to="https://discord.gg/g7PzMaM" target="_blank">
+                                        <i className="fa fa-commenting-o " /> Discord (Chat)
+                                    </Link>
+                                    <Link className="dropdown-item" to="http://wiki.starwarsnwn.com/" target="_blank">
+                                        <i className="fa fa-wikipedia-w " /> Wiki
+                                    </Link>
+                                </div>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/downloads">
-                                    <i className="fa fa-download fa-lg" /> Downloads
+                            <li className="nav-item dropdown">
+                                <Link id="aboutDropdown" className="nav-link dropdown-toggle" to="#" data-toggle="dropdown" role="button">
+                                    <i className="fa fa-user " /> About
                                 </Link>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="http://forums.starwarsnwn.com/">
-                                    <i className="fa fa-th-list fa-lg" /> Forums
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="https://discord.gg/g7PzMaM" target="_blank">
-                                    <i className="fa fa-commenting-o fa-lg" /> Discord (Chat)
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="http://czs.wikia.com/" target="_blank">
-                                    <i className="fa fa-wikipedia-w fa-lg" /> Wiki
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="https://github.com/zunath/Star-Wars-Legends-of-the-Old-Republic-NWN" target="_blank">
-                                    <i className="fa fa-code fa-lg" /> Source Code
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/team">
-                                    <i className="fa fa-th-list fa-lg" /> Team
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/about-nwn">
-                                    <i className="fa fa-info-circle fa-lg" /> About NWN
-                                </Link>
+                                <div className="dropdown-menu">
+                                    <Link className="dropdown-item" to="/team-members">
+                                        <i className="fa fa-user-circle-o" /> Team Members
+                                    </Link>
+                                    <Link className="dropdown-item" to="/neverwinter-nights" target="_blank">
+                                        <i className="fa fa-external-link" /> Buy Neverwinter Nights
+                                    </Link>
+                                </div>
                             </li>
 
                             {renderAdmin()}
@@ -123,12 +137,12 @@ export default class Header extends React.Component<any, any> {
 
                                 <li className="nav-item pull-right">
                                     <a className="nav-link" href="/Authorization/Login">
-                                        <i className="fa fa-sign-in fa-lg" /> Login (With Discord)
+                                        <i className="fa fa-sign-in " /> Login (With Discord)
                                     </a>
                                 </li> :
                                 <li className="nav-item dropdown">
                                     <Link id="userDropdown" className="nav-link dropdown-toggle" to="#" data-toggle="dropdown" role="button">
-                                        <i className="fa fa-user fa-lg" /> {this.state.Username}
+                                        <i className="fa fa-user " /> {this.state.Username}
                                     </Link>
                                     <div className="dropdown-menu">
                                         <Link className="dropdown-item" to="/user-profile">
--- SWLOR.Web/ClientApp/components/Home.tsx
+++ SWLOR.Web/ClientApp/components/Home.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 
 export default class Home extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+    }
+    
     render() {
+
+
         return (
             <div>
-
-
+                <div className="row">
+                    <div className="embed-responsive embed-responsive-21by9">
+                        <iframe className="embed-responsive-item" src="http://www.starwarsnwn.com/Intro" />
+                    </div>
+                </div>
             </div>);
 
 
