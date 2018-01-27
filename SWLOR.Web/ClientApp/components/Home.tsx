50166007e75e4e7fd9a0f2f4cf2742a32a7c91ae
zunath <coolty3001@yahoo.com>
2018-01-28 00:27:36 +0800

    Move the disclaimer to the home page since it doesn't look good anywhere else.

--- SWLOR.Web/ClientApp/components/Home.tsx
+++ SWLOR.Web/ClientApp/components/Home.tsx
@@ -16,6 +16,17 @@ export default class Home extends React.Component<any, any> {
                         <iframe className="embed-responsive-item" src="http://www.starwarsnwn.com/Intro" />
                     </div>
                 </div>
+
+                <div className="container">
+                    <div className="row">&nbsp;</div>
+                    <footer>
+                        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                            <span>
+                                Star Wars: Legends of the Old Republic is a free mod developed for Neverwinter Nights. Star Wars, the Expanded Universe, all logos, characters, artwork, stories, information, names, and other elements associated thereto, are sole and exclusive property of Lucasfilm Limited. We are a completely free, open source non-profit project and operate under the definition of "fair use" under United States copyright laws.
+                            </span>
+                        </nav>
+                    </footer>
+                </div>
             </div>);
 
 
